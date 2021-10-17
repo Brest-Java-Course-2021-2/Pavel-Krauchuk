4b01627a6003f9700ba75f16752e5cbd864b1326
krauchukp <krauchukp@gmail.com>
2021-10-17 19:41:28 +0300

    First update The operating mode is set

--- src/main/java/com/epam/brest/Main.java
+++ src/main/java/com/epam/brest/Main.java
@@ -1,7 +1,24 @@
 package com.epam.brest;
 
+import com.epam.brest.model.ReadData;
+import com.epam.brest.model.Status;
+import com.epam.brest.model.StatusType;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        //result = weight*pricePerKg + km*pricePerKm;
+        //scanner =
+        //prop / json / csv
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            Status currentStatus = new ReadData(scanner);
+            while (currentStatus.getType() != StatusType.EXIT) {
+                System.out.println("current type: " + currentStatus.getType());
+                currentStatus = currentStatus.handle();
+            }
+        }
     }
+
 }
---
+++ src/main/java/com/epam/brest/calc/CalcImp.java
@@ -0,0 +1,10 @@
+package com.epam.brest.calc;
+
+import java.math.BigDecimal;
+
+
+public class CalcImp {
+    public static BigDecimal handle(BigDecimal weight, BigDecimal pricePerKg, BigDecimal length, BigDecimal pricePerKm){
+        return weight.multiply(pricePerKg).add(length.multiply(pricePerKm));
+    }
+}
